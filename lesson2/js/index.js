80f1e06f3edfb0ed74b7dae1e44cf1f75abe04a7
Ilia Filimonov <9991571155i@gmail.com>
2023-01-17 00:25:57 +0700

    fixed structure of packages

--- lesson2/js/index.js
+++ lesson2/js/index.js
@@ -6,5 +6,4 @@ let htmlForFat = renderTableRow("Fat", "5g");
 
 const tbody = document.querySelector("#nutrition-table tbody");
 tbody.insertAdjacentHTML("beforeend", htmlForCarbs);
-tbody.insertAdjacentHTML("beforeend", htmlForProtein);
-tbody.insertAdjacentHTML("beforeend", htmlForFat);
+
