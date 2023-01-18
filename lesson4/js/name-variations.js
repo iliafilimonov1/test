7cca203893a587af4465d49a8da3a9c7ae6bed04
Ilia Filimonov <9991571155i@gmail.com>
2023-01-19 00:44:50 +0700

    lesson4. решение задачи с использованием классов

---
+++ lesson4/js/name-variations.js
@@ -0,0 +1,18 @@
+export default class NameVariations {
+  /** @param {string} name */
+  constructor(name) {
+    this.name = name;
+  }
+
+  getNumberOfChars() {
+    return this.name.length;
+  }
+
+  getLower() {
+    return this.name.toLowerCase();
+  }
+
+  getUpper() {
+    return this.name.toUpperCase();
+  }
+}
